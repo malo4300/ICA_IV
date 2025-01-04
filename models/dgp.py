7bce7baa14ad019eddd066272ff984cfbe773b16
Marvin Lob <malo4300@outlook.de>
2025-01-04 09:45:34 +0100

    changed order to topological order, now all simulations that try to extract the treatment effect are outdated

--- models/dgp.py
+++ models/dgp.py
@@ -22,12 +22,12 @@ class dgp():
 
         # generate data
         self.signals = np.zeros((n, J))
-        self.signals[:, 0] = self.signal_T
-        self.signals[:, 1] = self.signal_Y
-        self.signals[:, 2] = self.signal_U
+        self.signals[:, 0] = self.signal_U
         if self.no_controls > 0:
-            self.signals[:, 3:] = self.signal_X
+            self.signals[:, 1:(self.no_controls+1)] = self.signal_X
         self.data = np.zeros((n, J))
+        self.signals[:, J-2] = self.signal_T
+        self.signals[:, J-1] = self.signal_Y
         for j in range(J):
             self.data[:, j] = self.mixing_matrix[j, :] @ self.signals.T
 
@@ -35,9 +35,9 @@ class dgp():
             self.data += np.random.normal(loc=self.noise['loc'], scale=self.noise["scale"], size=(n, J))
         
         # the treatment effect is in 1,0
-        self.treatment_effect = self.coef_mat[1, 0]
+        self.treatment_effect = self.coef_mat[-1, J-2]
         # observed data is missing the confounder
-        obs_indices = [i for i in range(J) if i != 2]
+        obs_indices = range(1, J)
         self.data_observed = self.data[:, obs_indices]
         self.mixing_matrix_observed = self.mixing_matrix[obs_indices, :]
 
@@ -45,21 +45,22 @@ class dgp():
     def _generate_dag(self, J: int):
         self.adj_matrx = np.zeros((J, J)) # adjacency matrix at least 3 nodes
         # TODO: make this more general and allow for edges between controls 
-        # w.l.o.g. we can assume that the first node is the treatment and the second node is the outcome
-        self.adj_matrx[0,2] = 1 # confounder -> treatment
-        self.adj_matrx[1,2] = 1 # confounder -> outcome
-        self.adj_matrx[1,0] = 1 # treatment -> outcome
+        # w.l.o.g. we can assume that the matrix is in topological order meaning the confounder is the first node then controls then treatment (J-2) and outcome (J-1)
+        self.adj_matrx[J-2,0] = 1 # confounder -> treatment
+        self.adj_matrx[J-1,0] = 1 # confounder -> outcome
+        self.adj_matrx[J-1,J-2] = 1 # treatment -> outcome
         # build the adjacency matrix for controls
         self.no_controls = J - 3
-        for i in range(3, J):
-            self.adj_matrx[1, i] = 1
+        # controls -> outcome
+        for i in range(1, J-2):
+            self.adj_matrx[J-1, i] = 1
         
         # introduce additional edges
         if J > 3:
-            self.adj_matrx[0, 3] = 1 # control -> treatment
+            self.adj_matrx[J-2, 2] = 1 # control -> treatment
         if J > 4:
-            self.adj_matrx[4, 3] = 1 # control1 -> control
-            self.adj_matrx[J-2, J-1] = 1 # control1 -> outcome
+            self.adj_matrx[2, 1] = 1 # control1 -> control2
+            self.adj_matrx[3, 1] = 1 # control1 -> control3 
 
 
 
