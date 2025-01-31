3389787732f38df8835c049a6b307ff3132dcb2f
Marvin Lob <malo4300@outlook.de>
2025-01-31 19:46:22 +0100

    build pipeline for the IV test

---
+++ IV_on_ICA_ets.R
@@ -0,0 +1,82 @@
+
+####################################################################
+# implementation of instrument validity test developed in 
+# Burauel, Patrick F. "Evaluating Instrument Validity using the Principle of Independent Mechanisms." 
+# Journal of Machine Learning Research 24.176 (2023): 1-56.
+# https://jmlr.org/papers/v24/20-1287.html
+# abbreviated to B23 in the following
+####################################################################
+
+rm(list = ls()) # To clear all
+
+# load required packages
+packages <- c("boot",
+              "corpcor",
+              "MASS",
+              "parallel",
+              "pracma",
+              "knockoff")
+
+lapply(packages,require,character.only=TRUE)
+
+# load necessary functions
+source('fn_2SLS.R')
+source('estimate_kappa_bootstrap_wrapper.R')
+source('fn_complement.R')
+source('fn_complement_knockoff.R')
+source('fn_test_instrument_validity.R')
+source('estimate_confounding_via_kernel_smoothing.R')
+source('estimate_confounding_sigmas.R')
+
+
+B = 500 # number of bootstrap draws
+ncpus = 10
+synthetic_D_method = 'standard'
+kappa_method = 'sigmas'
+
+order_data = function(data){
+  ordered_data = matrix(0, nrow = nrow(data), ncol = ncol(data))
+  ordered_data[,1] = data[, ncol(data)] # Y has to be the first column, in data it is the last
+  ordered_data[,2:(ncol(data)-1)] = as.matrix(data[,1:3]) # controlls in the middle
+  ordered_data[,ncol(data)] = data[, ncol(data)-1] # Treatment as last
+  return(ordered_data)
+}
+
+get_path = function(str, i){
+  return(paste(str,i, ".csv", sep = "" ))
+}
+
+p_values = function(ordered_data, signals){
+  p_val = rep(0,6)
+  for (i in 1:length(names(signals))) {
+    p_val[i] = fn_test_instrument_validity(ordered_data, signals[[names(signals)[i]]], B, 
+                                       ncpus, 
+                                       kappa_method,
+                                       synthetic_D_method)$pseudo_p
+  }
+  return(p_val)
+}
+
+
+results = matrix(0, nrow = 100, ncol = 7)
+pb = txtProgressBar(min = 1, max = 100, initial = 1, style = 3) 
+for (i in 1:100) {
+  sg_path = get_path("sim_data/signals/estimated_signals_", i)
+  dt_path = get_path("sim_data/data/data_obs_", i)
+  data =  read.csv(dt_path, header = F)
+  signals = read.csv(sg_path, header = F)
+  ordered_data = order_data(data)
+  results[i,1] = i
+  results[i,2:7] = p_values(ordered_data, signals)
+  setTxtProgressBar(pb,i)
+  
+}
+i = 0
+sg_path = get_path("sim_data/signals/estimated_signals_", i)
+dt_path = get_path("sim_data/data/data_obs_", i)
+data =  read.csv(dt_path, header = F)
+signals = read.csv(sg_path, header = F)
+ordered_data = order_data(data)
+results[100,1] = i
+results[100,2:7] = p_values(ordered_data, signals)
+write.csv(results, file = "p_values_ICA.csv")
